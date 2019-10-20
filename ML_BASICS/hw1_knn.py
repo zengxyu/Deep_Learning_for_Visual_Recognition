b1e30535c709e825c4a9dd127999922db8eddbcb
xiangyu zeng <s6xizeng@uni-bonn.de>
2019-10-20 19:49:29 +0200

    Init

---
+++ ML_BASICS/hw1_knn.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on  
+
+@author: fame
+"""
+ 
+import numpy as np 
+ 
+
+def compute_euclidean_distances( X, Y ) :
+    """
+    Compute the Euclidean distance between two matricess X and Y  
+    Input:
+    X: N-by-D numpy array 
+    Y: M-by-D numpy array 
+    
+    Should return dist: M-by-N numpy array   
+    """  
+ 
+
+def predict_labels( dists, labels, k=1):
+    """
+    Given a Euclidean distance matrix and associated training labels predict a label for each test point.
+    Input:
+    dists: M-by-N numpy array 
+    labels: is a N dimensional numpy array
+    
+    Should return  pred_labels: M dimensional numpy array
+    """
+     
---
+++ ML_BASICS/hw1_linear.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on  
+
+@author: fame
+"""
+
+import numpy as np 
+ 
+
+def predict(X,W,b):  
+    """
+    implement the function h(x, W, b) here  
+    X: N-by-D array of training data 
+    W: D dimensional array of weights
+    b: scalar bias
+
+    Should return a N dimensional array  
+    """
+    
+ 
+def sigmoid(a): 
+    """
+    implement the sigmoid here
+    a: N dimensional numpy array
+
+    Should return a N dimensional array  
+    """
+    
+
+def l2loss(X,y,W,b):  
+    """
+    implement the L2 loss function
+    X: N-by-D array of training data 
+    y: N dimensional numpy array of labels
+    W: D dimensional array of weights
+    b: scalar bias
+
+    Should return three variables: (i) the l2 loss: scalar, (ii) the gradient with respect to W, (iii) the gradient with respect to b
+     """
+
+
+def train(X,y,W,b, num_iters=1000, eta=0.001):  
+    """
+    implement the gradient descent here
+    X: N-by-D array of training data 
+    y: N dimensional numpy array of labels
+    W: D dimensional array of weights
+    b: scalar bias
+    num_iters: (optional) number of steps to take when optimizing
+    eta: (optional)  the stepsize for the gradient descent
+
+    Should return the final values of W and b    
+     """
+
+
+ 
